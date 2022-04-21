01d872a47a365c969d6425c2fd132b1495623bfb
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-21 18:37:01 +0300

    feat: перенести логику вызова приложения в App и добавить опцию --format

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,35 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Prints the diff between two files to STDOUT.")
+public class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filePath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: ${DEFAULT-VALUE}]", defaultValue = "stylish")
+    private String format;
+
+    @Override
+    public Integer call() throws Exception {
+        String diff = Differ.generate(filePath1, filePath2);
+        System.out.println(diff);
+        return 0;
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
 }
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,26 +1,12 @@
 package hexlet.code;
 
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
-
-import java.util.concurrent.Callable;
+public final class Differ {
 
     private Differ() {
     }
 
     public static String generate(String filePath1, String filePath2) {
         return "";
-    }
-
-    @Override
-    public Integer call() throws Exception {
-        return 0;
-    }
-
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new Differ()).execute(args);
-        System.exit(exitCode);
     }
 
 }
