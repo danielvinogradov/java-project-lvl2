0c0d1f0a5ad4135487f07ab2c58aaed2a1177157
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-21 18:35:58 +0300

    feat: запретить создавать инстансы Differ

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -6,15 +6,8 @@ import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-        description = "Prints the diff between two files to STDOUT.")
-public final class Differ implements Callable<Integer> {
-
-    @Parameters(index = "0", description = "Path to the first file.")
-    private String filePath1;
-
-    @Parameters(index = "1", description = "Path to the second file.")
-    private String filePath2;
+    private Differ() {
+    }
 
     public static String generate(String filePath1, String filePath2) {
         return "";
