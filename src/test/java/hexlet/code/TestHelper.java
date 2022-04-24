9879c92ca0b03346a1811632b043b1e06ef96773
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-24 11:48:01 +0300

    test: добавить TestHelper (константы с путями до файлов)

---
+++ src/test/java/hexlet/code/TestHelper.java
@@ -0,0 +1,7 @@
+package hexlet.code;
+
+public final class TestHelper {
+    public static final String FIXTURES_DIR = "./src/test/resources/hexlet/code/fixtures/";
+    public static final String FIXTURES_JSON_DIR = FIXTURES_DIR + "json/";
+    public static final String FIXTURES_YAML_DIR = FIXTURES_DIR + "yaml/";
+}
